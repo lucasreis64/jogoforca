7aafec9d5b64854da60c3a334150bc05770cd396
Lucas <lucasreis64@gmail.com>
2022-09-23 01:21:20 -0300

    Jogo completo, responsividade ok, falta componente

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { useState } from "react"
+import React, { useState } from 'react'
 import words from "./ auxiliaries/Words"
 import alfabet from "./ auxiliaries/Alfabet"
 import forca0 from "../images/forca0.png"
@@ -13,18 +13,22 @@ import forca6 from "../images/forca6.png"
 import "./style.css"
 import "./reset.css"
 
-export default function App () {
-/*     const inicialKey = <div key={index} className = "keyboard-key">{a}</div>
-    const functionalKey = <div key={index} onClick={()=>selectedKey(index, randomArr,underlineArr, randomStr)} className = "keyboard-key">{a}</div> */
+let gameStart=false;
+let chosenOne;
+let selected=[];
+alfabet.map((s, index)=>selected[index]='')
+
+function App () {console.log(selected)
     const [alfabets, setAlfabets] = useState(alfabet.map((a, index)=><div key={index} className = "keyboard-key">{a}</div>))
-    const [answer,setAnswer] = useState ('')
-    const [buttonUnderline, setButtonUnderline] = useState(<button onClick={()=>GameStart()}>Escolher palavra!</button>)
+    const [buttonUnderline, setButtonUnderline] = useState(<button onClick={()=>wordChooser()}>Escolher palavra!</button>)
     const [gibbet, setGibbet] = useState(<img src = {forca0} alt = ""/>)
     const gibbetArr = [forca0,forca1,forca2,forca3,forca4,forca5,forca6]
     const [inputButtom, setInputButtom] = useState(<button>Chutar</button>)
+    const [pesquisa, setPesquisa] = useState("");
     let victory = 0;
     let error = 0,trueError=0;
-    let chosenOne;
+    
+    
     
     function randomValue(min, max) {
         min = Math.ceil(min);
@@ -48,31 +52,41 @@ export default function App () {
         arrTwo.map((a,index)=>(a===arrOne[index])?victory++:true)
         if(victory===arrTwo.length){
             console.log('parabens')
-            setButtonUnderline(<div className = "buttom-underline"><button onClick={()=>GameStart()}>Escolher palavra!</button><div className="green">{arrTwo}</div></div>)
+            setButtonUnderline(<div className = "buttom-underline"><button onClick={()=>wordChooser()}>Escolher palavra!</button><div className="green">{arrTwo}</div></div>)
             setAlfabets(alfabet.map((a, index)=><div key={index} className = "keyboard-key">{a}</div>))
+            setPesquisa('');
         }
     }
 
     function verifyEqualInput(inputs, arrOne) {
-        console.log("inputs: ",inputs)
         console.log("arrOne",arrOne)
         const inputArr = inputs.split('')
+        let inputStr = arrOne.join('')
+        console.log(inputStr)
         console.log("inputArr",inputArr)
-        inputArr.map((a,index)=>(a===arrOne[index])?victory++:true)
+        let inputArrFormated = replaceAccent(inputStr)
+        inputArrFormated = inputArrFormated.split('')
+        console.log(". . . ",inputArrFormated)
+        inputArr.map((a,index)=>(a===arrOne[index]||a===inputArrFormated[index])?victory++:true)
+        console.log(victory)
         if(victory===inputArr.length && inputs!==''){
             console.log('parabens')
-            setButtonUnderline(<div className = "buttom-underline"><button onClick={()=>GameStart()}>Escolher palavra!</button><div className="green">{inputArr}</div></div>)
+            setButtonUnderline(<div className = "buttom-underline"><button onClick={()=>wordChooser()}>Escolher palavra!</button><div className="green">{arrOne}</div></div>)
             setAlfabets(alfabet.map((a, index)=><div key={index} className = "keyboard-key">{a}</div>))
+            setPesquisa('');
         }
-        if (inputs===''){return}
+        else if (inputs===''){return}
         else{
             console.log('wasted')
-            setButtonUnderline(<div className = "buttom-underline"><button onClick={()=>GameStart()}>Escolher palavra!</button><div className="red">{arrOne}</div></div>)
+            setButtonUnderline(<div className = "buttom-underline"><button onClick={()=>wordChooser()}>Escolher palavra!</button><div className="red">{arrOne}</div></div>)
             setAlfabets(alfabet.map((a, index)=><div key={index} className = "keyboard-key">{a}</div>))
+            setGibbet(<img src = {gibbetArr[6]} alt = ""/>)
+            setPesquisa('');
         }
     }
 
     function selectedKey (idx, arr, arr2, str) {
+        selected[idx]=alfabet[idx]
         victory = 0
         error = 0
         console.log(alfabet[idx])
@@ -81,9 +95,9 @@ export default function App () {
         const randomArr = randomArrCopy.split('')
         randomArr.map((a, index)=>(a===alfabet[idx])?arr2[index]=arr[index]:error++)
         if (error<randomArr.length){error = 0} else {trueError++}
-        setButtonUnderline(<div className = "buttom-underline"><button onClick={()=>GameStart()}>Escolher palavra!</button><div>{arr2}</div></div>)
+        setButtonUnderline(<div className = "buttom-underline"><button onClick={()=>wordChooser()}>Escolher palavra!</button><div>{arr2}</div></div>)
         setGibbet(<img src = {gibbetArr[trueError]} alt = ""/>)
-        setAlfabets(alfabet.map((a,index)=>(a===alfabet[idx])?<div key={index} className = "keyboard-key">{a}</div>:<div key={index} onClick={()=>selectedKey(index, arr, arr2, str)} className = "keyboard-key">{a}</div>))
+        setAlfabets(alfabet.map((a,index)=>(a===alfabet[idx]||a===selected[index])?<div key={index} className = "keyboard-key selected">{a}</div>:<div key={index} onClick={()=>selectedKey(index, arr, arr2, str)} className = "keyboard-key">{a}</div>))
         console.log(arr2)
         console.log(randomArr)
         verifyEqual(arr2,arr)
@@ -92,28 +106,31 @@ export default function App () {
 
         if(trueError===6){
             console.log('wasted')
-            setButtonUnderline(<div className = "buttom-underline"><button onClick={()=>GameStart()}>Escolher palavra!</button><div className="red">{arr}</div></div>)
+            setButtonUnderline(<div className = "buttom-underline"><button onClick={()=>wordChooser()}>Escolher palavra!</button><div className="red">{arr}</div></div>)
             setAlfabets(alfabet.map((a, index)=><div key={index} className = "keyboard-key">{a}</div>))
+            setPesquisa('');
         }
     }
     
     function wordChooser () {
+        selected = selected.map((s)=>s='')
+        gameStart=true;
+        console.log(gameStart)
         trueError=0
         setGibbet(<img src = {gibbetArr[trueError]} alt = ""/>)
         const random = randomValue (0, words.length)
         console.log(random)
         const randomStr = words[random]
         chosenOne = randomStr.split('')
-        console.log("chosenOne",chosenOne)
+        console.log("chosenOne ",chosenOne, "pesquisa ", pesquisa)
         const underlineArr = chosenOne.map((u)=>u='_')
-        
-        setButtonUnderline(<div className = "buttom-underline"><button onClick={()=>GameStart()}>Escolher palavra!</button><div>{underlineArr}</div></div>)
+        setInputButtom(<button onClick={()=>verifyEqualInput(pesquisa, chosenOne)}>Chutar</button>)
+        setButtonUnderline(<div className = "buttom-underline"><button onClick={()=>wordChooser()}>Escolher palavra!</button><div>{underlineArr}</div></div>)
         setAlfabets(alfabet.map((a, index)=><div key={index} onClick={()=>selectedKey(index, chosenOne,underlineArr, randomStr)} className = "keyboard-key">{a}</div>))
-        setInputButtom(<button onClick={verifyEqualInput(answer, chosenOne)}>Chutar</button>)
     }
 
-    function GameStart () {
-        wordChooser();
+    function atribuirValor(e) {
+        setPesquisa(e.target.value)
     }
 
     return(
@@ -126,11 +143,13 @@ export default function App () {
                 {alfabets}
             </div>
             <div className="guess">
-                <label>Já sei a palavra!</label>
-                <input type = "text" value={answer} onChange={(e) => setAnswer(e.target.value)}/>
-                {inputButtom}
+                <div class="label">Já sei a palavra!</div>
+                <input value={pesquisa} onChange={atribuirValor}/>
+                <button onClick={(gameStart===true)?()=>verifyEqualInput(pesquisa, chosenOne):()=>console.log(gameStart)}>Chutar</button>
             </div>
         </div>
     )
 }
+
+export default App
 
